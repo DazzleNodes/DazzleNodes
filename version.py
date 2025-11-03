01e84c30c6f4ae9ec16e5f9419130a3df31ef098
Dustin <6962246+djdarcy@users.noreply.github.com>
2025-11-02 19:45:08 -0500

    Improve pre-commit hook documentation explaining two-phase version update

--- version.py
+++ version.py
@@ -27,7 +27,7 @@ PHASE = "alpha"  # Options: None, "alpha", "beta", "rc1", "rc2", etc.
 # DO NOT EDIT THIS LINE MANUALLY
 # Note: Hash reflects the commit this version builds upon (HEAD at commit time)
 # The hash will be one commit behind after the commit is created (git limitation)
-__version__ = "0.1.0-alpha_main_19-20251102-7093590"
+__version__ = "0.1.0-alpha_main_22-20251102-a10f217"
 
 
 def get_version():
